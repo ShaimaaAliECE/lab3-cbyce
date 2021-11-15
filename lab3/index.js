fea319663cdb74f73ba035009e71a87ef55817b0
cbyce <cbyce@uwo.ca>
2021-11-15 13:28:01 -0500

    Minor changes, walking through initial set up

--- lab3/index.js
+++ lab3/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.static('static'));
 
 // dynamic handling
+app.get('/guest', (req, res) => {
 
+});
+
+app.get('/login', (req, res) => {
+    res.send('<div>Hello</div>');
+});
 
 //app.listen(2000);
